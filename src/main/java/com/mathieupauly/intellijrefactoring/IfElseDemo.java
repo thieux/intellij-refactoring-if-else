8e8c762832229b06c98e1cae00e380fda28de0a3
Mathieu Pauly <mathieu.pauly@gmail.com>
2016-10-03 22:15:29 +0200

    Remove duplicate calls to calculate

--- src/main/java/com/mathieupauly/intellijrefactoring/IfElseDemo.java
+++ src/main/java/com/mathieupauly/intellijrefactoring/IfElseDemo.java
@@ -7,17 +7,14 @@ public class IfElseDemo {
     public double calculateInsurance(double income) {
         if (income <= 10000) {
             strategy = new LowInsuranceStrategy();
-            return strategy.calculate(income);
         } else if (income <= 30000) {
             strategy = new MediumInsuranceStrategy();
-            return strategy.calculate(income);
         } else if (income <= 60000) {
             strategy = new HighInsuranceStrategy();
-            return strategy.calculate(income);
         } else {
             strategy = new VeryHighInsuranceStrategy();
-            return strategy.calculate(income);
         }
+        return strategy.calculate(income);
     }
 
 }
