99c67f6b2662f545b1f62c714505964f3078d3aa
Mathieu Pauly <mathieu.pauly@gmail.com>
2016-10-03 22:34:10 +0200

    Put declaration of variable at the beginning

--- src/main/java/com/mathieupauly/intellijrefactoring/IfElseDemo.java
+++ src/main/java/com/mathieupauly/intellijrefactoring/IfElseDemo.java
@@ -3,25 +3,28 @@ package com.mathieupauly.intellijrefactoring;
 public class IfElseDemo {
 
     public double calculateInsurance(double income) {
+        double adjustement;
+        double weight;
+        double constant;
         if (income <= 10000) {
-            double adjustement = 0;
-            double weight = 0.365;
-            double constant = 0;
+            adjustement = 0;
+            weight = 0.365;
+            constant = 0;
             return (income - adjustement) * weight + constant;
         } else if (income <= 30000) {
-            final int adjustement = 10000;
-            final double weight = 0.2;
-            final int constant = 35600;
+            adjustement = 10000;
+            weight = 0.2;
+            constant = 35600;
             return (income - adjustement) * weight + constant;
         } else if (income <= 60000) {
-            final int adjustement = 30000;
-            final double weight = 0.1;
-            final int constant = 76500;
+            adjustement = 30000;
+            weight = 0.1;
+            constant = 76500;
             return (income - adjustement) * weight + constant;
         } else {
-            final int adjustement = 60000;
-            final double weight = 0.02;
-            final int constant = 105600;
+            adjustement = 60000;
+            weight = 0.02;
+            constant = 105600;
             return (income - adjustement) * weight + constant;
         }
     }
