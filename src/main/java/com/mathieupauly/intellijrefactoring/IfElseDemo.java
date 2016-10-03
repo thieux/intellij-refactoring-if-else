439df9f3f1494c03f3d7d4849cb5b9d87eda5882
Mathieu Pauly <mathieu.pauly@gmail.com>
2016-10-03 22:36:04 +0200

    Remove duplicated insurance calculation formula

--- src/main/java/com/mathieupauly/intellijrefactoring/IfElseDemo.java
+++ src/main/java/com/mathieupauly/intellijrefactoring/IfElseDemo.java
@@ -10,22 +10,19 @@ public class IfElseDemo {
             adjustement = 0;
             weight = 0.365;
             constant = 0;
-            return (income - adjustement) * weight + constant;
         } else if (income <= 30000) {
             adjustement = 10000;
             weight = 0.2;
             constant = 35600;
-            return (income - adjustement) * weight + constant;
         } else if (income <= 60000) {
             adjustement = 30000;
             weight = 0.1;
             constant = 76500;
-            return (income - adjustement) * weight + constant;
         } else {
             adjustement = 60000;
             weight = 0.02;
             constant = 105600;
-            return (income - adjustement) * weight + constant;
         }
+        return (income - adjustement) * weight + constant;
     }
 }
